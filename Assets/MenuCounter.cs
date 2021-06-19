e7c0f50b95eff7820750502dc51fb4ad0c0c8a36
GeunTae KIM <k031900@gmail.com>
2021-06-19 17:55:05 +0900

    want happy

---
+++ Assets/MenuCounter.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuCounter : MonoBehaviour
+{
+    public int thisMenuCode;
+    public int counter = 0;
+    public GameObject[] counterSprite;
+    public int selectedMenuCode;
+
+
+    public void thisClick(int code)
+    {
+        if (selectedMenuCode != code)
+        {
+            ResetCounter();
+            selectedMenuCode = code;
+            Count();
+        }
+        else
+        {
+            if (counter < 3)
+                Count();
+            else if (counter == 3)
+            {
+                ResetCounter();
+            }
+        }
+    }
+
+    public void Count()
+    {
+        counterSprite[counter].SetActive(true);
+        counter++;
+    }
+
+    public void ResetCounter()
+    {
+        counter = 0;
+
+        foreach (GameObject x in counterSprite)
+        {
+            x.SetActive(false);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Ingredients.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Ingredients : MonoBehaviour
+{
+    public GameObject[] ingredients;
+    public int[] ingredNums;
+
+    public void Click(int code)
+    {
+        if (ingredNums[code] < 3)
+        {
+            ingredNums[code]++;
+            ingredients[code].transform.GetChild(0).gameObject.SetActive(true); //outline 활성화
+            ingredients[code].transform.GetChild(1).gameObject.SetActive(true); //N 활성화
+            ingredients[code].transform.GetChild(2).gameObject.SetActive(true); //x 활성화
+            ingredients[code].transform.GetChild(1).gameObject.GetComponent<TMP_Text>().text = ingredNums[code].ToString();
+
+        }else if (ingredNums[code] == 3)
+        {
+            ingredients[code].transform.GetChild(0).gameObject.SetActive(false);
+            ingredients[code].transform.GetChild(1).gameObject.SetActive(false);
+            ingredients[code].transform.GetChild(2).gameObject.SetActive(false);
+            ingredNums[code] = 0;
+        }
+
+        //ingredNums[code]++;
+    }
+}
